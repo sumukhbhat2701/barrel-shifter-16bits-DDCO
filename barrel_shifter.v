25c4c47ed7187d441962f814e0ba8b97d12add59
Sumukh Raju Bhat <65952306+sumukhbhat2701@users.noreply.github.com>
2020-11-04 22:42:47 +0530

    Update barrel_shifter.v

--- barrel_shifter.v
+++ barrel_shifter.v
@@ -189,6 +189,6 @@ endmodule
 module barrel_shifter16(input wire [15:0] i,input wire shift_sel,s0,s1,s2,output wire [15:0] o0,o1)
 	wire [15:0] l,r;
 	demux1_2_16(i,shift_sel,l,r);
-	barrel_shifter_left16(l,s0,s1,s2,o0);
-	barrel_shifter_right16(r,s0,s1,s2,o1);
+	barrel_shifter_left16(l,s0,s1,s2,s3,o0);
+	barrel_shifter_right16(r,s0,s1,s2,s3,o1);
 endmodule
